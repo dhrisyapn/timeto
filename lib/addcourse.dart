8c66d9ab36936506108c73211fb8e8fdb7251a2b
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-10-17 11:51:54 +0530

    app bar added

--- lib/addcourse.dart
+++ lib/addcourse.dart
@@ -11,6 +11,17 @@ class AddCourse extends StatefulWidget {
 class _AddCourseState extends State<AddCourse> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        scrolledUnderElevation: 0,
+        title: Image.asset(
+          'assets/name.png',
+          height: 20,
+        ),
+      ),
+    );
   }
 }
