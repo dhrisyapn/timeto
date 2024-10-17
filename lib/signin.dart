7650dff0a3bf4d108629dae8c36daf75a3ec8d1a
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-10-17 19:13:24 +0530

    sign in function added

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:timeto/home.dart';
 
 class SigninPage extends StatefulWidget {
   const SigninPage({super.key});
@@ -8,6 +10,40 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  //sign in function
+  Future<void> signInWithEmailPassword() async {
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(
+        email: emailcontroller.text.trim(),
+        password: passwordcontroller.text.trim(),
+      );
+
+      // Check if sign in was successful
+      if (userCredential.user != null) {
+        // Navigate to your target page if login is successful
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+
+                    // Change this to the page you want to navigate to after login
+                    const HomePage()),
+            (route) => false);
+      }
+    } on FirebaseAuthException catch (e) {
+      // Handle errors
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
   bool visible = true;
   var eyeicon = const Icon(Icons.visibility);
   void toggleicon() {
@@ -26,171 +62,184 @@ class _SigninPageState extends State<SigninPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(left: 30, right: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              'assets/profile-icon.gif',
-              height: 80,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Image.asset(
-              'assets/name.png',
-              height: 28,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Sign in to your account.',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w400,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height *
+                    0.25, // 50% of screen height
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Email address',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 14,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w400,
+              Image.asset(
+                'assets/profile-icon.gif',
+                height: 80,
               ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              decoration: ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(width: 1, color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
+              SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                'assets/name.png',
+                height: 28,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Sign in to your account.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
                 ),
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                  hintText: 'Your email here',
-                  hintStyle: TextStyle(
-                    color: Colors.white.withOpacity(0.4000000059604645),
-                    fontSize: 14,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w400,
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Email address',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(width: 1, color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                style: const TextStyle(
-                  color: Color(0xFFffffff),
-                  fontSize: 15,
-                ),
-                cursorColor: const Color(0xFFffffff),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Password',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 14,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              decoration: ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(width: 1, color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 10, top: 13),
-                  hintText: 'Your secret here',
-                  hintStyle: TextStyle(
-                    color: Colors.white.withOpacity(0.4000000059604645),
-                    fontSize: 14,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w400,
+                child: TextField(
+                  controller: emailcontroller,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                    hintText: 'Your email here',
+                    hintStyle: TextStyle(
+                      color: Colors.white.withOpacity(0.4000000059604645),
+                      fontSize: 14,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                  suffixIcon: IconButton(
-                      onPressed: toggleicon,
-                      icon: eyeicon,
-                      color: const Color(0xffffffff)),
+                  style: const TextStyle(
+                    color: Color(0xFFffffff),
+                    fontSize: 15,
+                  ),
+                  cursorColor: const Color(0xFFffffff),
                 ),
-                style: const TextStyle(
-                  color: Color(0xFFffffff),
-                  fontSize: 15,
-                ),
-                obscureText: visible,
-                cursorColor: const Color(0xFFffffff),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Forgot Password?',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                    height: 0,
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Password',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(width: 1, color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 333,
-              height: 50,
-              decoration: ShapeDecoration(
-                color: Color(0xFF8CC1A9),
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(width: 1, color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
+                child: TextField(
+                  controller: passwordcontroller,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(left: 10, top: 13),
+                    hintText: 'Your secret here',
+                    hintStyle: TextStyle(
+                      color: Colors.white.withOpacity(0.4000000059604645),
+                      fontSize: 14,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                    suffixIcon: IconButton(
+                        onPressed: toggleicon,
+                        icon: eyeicon,
+                        color: const Color(0xffffffff)),
+                  ),
+                  style: const TextStyle(
+                    color: Color(0xFFffffff),
+                    fontSize: 15,
+                  ),
+                  obscureText: visible,
+                  cursorColor: const Color(0xFFffffff),
                 ),
               ),
-              child: Center(
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      height: 0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  signInWithEmailPassword();
+                },
+                child: Container(
+                  width: 333,
+                  height: 50,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF8CC1A9),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.white),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-          ],
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
         ),
       ),
     );
