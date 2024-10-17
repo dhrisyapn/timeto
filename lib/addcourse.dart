53ab86530bf0e69ac7609742c648988a0313a59c
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-10-17 14:30:21 +0530

    data added to add course page

--- lib/addcourse.dart
+++ lib/addcourse.dart
@@ -9,6 +9,141 @@ class AddCourse extends StatefulWidget {
 }
 
 class _AddCourseState extends State<AddCourse> {
+  final TextEditingController _subjectController = TextEditingController();
+  String? _selectedStaff;
+  final List<String> _staffNames = ['Staff 1', 'Staff 2', 'Staff 3'];
+  void _showAddSubjectDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Container(
+            width: 333,
+            height: 290,
+            decoration: ShapeDecoration(
+              color: Color(0xFF8CC1A9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Add New Subject',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Subject name',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Container(
+                    width: 293,
+                    height: 50,
+                    decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: TextField(
+                        controller: _subjectController,
+                        decoration: InputDecoration(
+                          hintText: 'New Subject Name',
+                          hintStyle:
+                              TextStyle(color: Colors.white, fontSize: 14),
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 293,
+                    height: 50,
+                    decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: DropdownButtonFormField<String>(
+                        value: _selectedStaff,
+                        items: _staffNames.map((String staff) {
+                          return DropdownMenuItem<String>(
+                            value: staff,
+                            child: Text(staff),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedStaff = newValue;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Select Staff',
+                          hintStyle:
+                              TextStyle(color: Colors.white, fontSize: 14),
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        dropdownColor: Color(0xFF8CC1A9),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 293,
+                    height: 50,
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Add subject',
+                        style: TextStyle(
+                          color: Color(0xFF2A7A85),
+                          fontSize: 14,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +155,115 @@ class _AddCourseState extends State<AddCourse> {
         title: Image.asset(
           'assets/name.png',
           height: 20,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                'Add new course',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Course name',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400,
+                height: 0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1, color: Colors.white),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                  hintText: 'New course name',
+                  hintStyle: TextStyle(
+                    color: Colors.white.withOpacity(0.4000000059604645),
+                    fontSize: 14,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                style: const TextStyle(
+                  color: Color(0xFFffffff),
+                  fontSize: 15,
+                ),
+                cursorColor: const Color(0xFFffffff),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Subjects',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: _showAddSubjectDialog,
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(width: 1, color: Color(0xFF8CC1A9)),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.add,
+                      color: Color(0xFFffffff),
+                    ),
+                    Text(
+                      'Add new Subject',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w300,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
