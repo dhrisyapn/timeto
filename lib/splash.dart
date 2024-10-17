c1933cbb19991b9484aa38f6fda4e6261b51bc82
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-10-17 19:21:39 +0530

    Update splash.dart

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:timeto/home.dart';
 import 'package:timeto/signin.dart';
+import 'package:timeto/transitions.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,10 +15,24 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 5), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const SigninPage()),
-      );
+      if (mounted) {
+        // Check if the user is logged in
+        User? user = FirebaseAuth.instance.currentUser;
+        if (user != null) {
+          // User is logged in, navigate to HomePage
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+            (Route<dynamic> route) => false,
+          );
+        } else {
+          // User is not logged in, navigate to SignInPage
+          Navigator.pushAndRemoveUntil(
+              context,
+              DissolvePageRoute(page: SigninPage()),
+              (Route<dynamic> route) => false);
+        }
+      }
     });
   }
 
