944c90904e8c9fb0cc0cf414ddf552a98fa21d2a
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-10-17 18:57:48 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:timeto/addcourse.dart';
+import 'package:timeto/course.dart';
 import 'package:timeto/firebase_options.dart';
+import 'package:timeto/home.dart';
 import 'package:timeto/signin.dart';
 import 'package:timeto/splash.dart';
 
@@ -33,7 +36,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF2A7A85),
         useMaterial3: true,
       ),
-      home: SigninPage(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timeto/signin.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,6 +9,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SigninPage()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
