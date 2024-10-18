178f5f6382ea5780319c22dda715bb0c0147eac0
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-10-18 18:56:50 +0530

    course list added

--- lib/addcourse.dart
+++ lib/addcourse.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -8,10 +10,45 @@ class AddCourse extends StatefulWidget {
   State<AddCourse> createState() => _AddCourseState();
 }
 
+class createdsubject {
+  final String name;
+  final String staff;
+  createdsubject(this.name, this.staff);
+}
+
 class _AddCourseState extends State<AddCourse> {
   final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _courseController = TextEditingController();
+  final String email = FirebaseAuth.instance.currentUser!.email!;
   String? _selectedStaff;
-  final List<String> _staffNames = ['Staff 1', 'Staff 2', 'Staff 3'];
+  List<Map<String, dynamic>> _staffData = [];
+  List<createdsubject> createdsubjects = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchStaffData();
+  }
+
+  void _fetchStaffData() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference staffCollection =
+        firestore.collection('users').doc(email).collection('staff');
+
+    QuerySnapshot querySnapshot =
+        await staffCollection.where('course', isEqualTo: 'no course').get();
+
+    List<Map<String, dynamic>> staffData = querySnapshot.docs.map((doc) {
+      return {
+        'name': doc['name'],
+      };
+    }).toList();
+
+    setState(() {
+      _staffData = staffData;
+    });
+  }
+
   void _showAddSubjectDialog() {
     showDialog(
       context: context,
@@ -22,7 +59,7 @@ class _AddCourseState extends State<AddCourse> {
           ),
           child: Container(
             width: 333,
-            height: 290,
+            // height: 290,
             decoration: ShapeDecoration(
               color: Color(0xFF8CC1A9),
               shape: RoundedRectangleBorder(
@@ -32,15 +69,17 @@ class _AddCourseState extends State<AddCourse> {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text(
-                    'Add New Subject',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  Center(
+                    child: Text(
+                      'Add New Subject',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                   SizedBox(height: 20),
@@ -91,10 +130,10 @@ class _AddCourseState extends State<AddCourse> {
                       padding: const EdgeInsets.symmetric(horizontal: 10),
                       child: DropdownButtonFormField<String>(
                         value: _selectedStaff,
-                        items: _staffNames.map((String staff) {
+                        items: _staffData.map((data) {
                           return DropdownMenuItem<String>(
-                            value: staff,
-                            child: Text(staff),
+                            value: data['name'],
+                            child: Text('${data['name']} '),
                           );
                         }).toList(),
                         onChanged: (String? newValue) {
@@ -114,23 +153,35 @@ class _AddCourseState extends State<AddCourse> {
                     ),
                   ),
                   SizedBox(height: 20),
-                  Container(
-                    width: 293,
-                    height: 50,
-                    decoration: ShapeDecoration(
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                  GestureDetector(
+                    onTap: () {
+                      String subjectName = _subjectController.text;
+                      if (subjectName.isNotEmpty && _selectedStaff != null) {
+                        createdsubjects
+                            .add(createdsubject(subjectName, _selectedStaff!));
+                        _subjectController.clear();
+                        Navigator.of(context).pop();
+                        setState(() {});
+                      }
+                    },
+                    child: Container(
+                      width: 293,
+                      height: 50,
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Add subject',
-                        style: TextStyle(
-                          color: Color(0xFF2A7A85),
-                          fontSize: 14,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w500,
+                      child: Center(
+                        child: Text(
+                          'Add subject',
+                          style: TextStyle(
+                            color: Color(0xFF2A7A85),
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
                     ),
@@ -155,6 +206,12 @@ class _AddCourseState extends State<AddCourse> {
         title: Image.asset(
           'assets/name.png',
           height: 20,
+        ),
+        leading: GestureDetector(
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
         ),
       ),
       body: Padding(
@@ -199,6 +256,7 @@ class _AddCourseState extends State<AddCourse> {
                 ),
               ),
               child: TextField(
+                controller: _courseController,
                 decoration: InputDecoration(
                   border: InputBorder.none,
                   contentPadding: EdgeInsets.symmetric(horizontal: 10),
@@ -264,31 +322,121 @@ class _AddCourseState extends State<AddCourse> {
               ),
             ),
             SizedBox(
-              height: MediaQuery.of(context).size.height *
-                  0.50, // 10% of screen height
+              height: 10,
             ),
-            Container(
-              width: 333,
-              height: 50,
-              decoration: ShapeDecoration(
-                color: Color(0xFF8CC1A9),
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(width: 1, color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: createdsubjects.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Color(0xFF8CC1A9)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          createdsubjects[index].name,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Row(
+                          children: [
+                            Text(
+                              createdsubjects[index].staff,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                createdsubjects.removeAt(index);
+                                setState(() {});
+                              },
+                              child: Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {
+                // save _courseController.text to collection users doc email subcollection course
+
+                FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+                CollectionReference courseCollection = firestore
+                    .collection('users')
+                    .doc(email)
+                    .collection('course');
+
+                courseCollection.add({
+                  'name': _courseController.text,
+                }).then((value) {
+                  createdsubjects.forEach((element) {
+                    courseCollection.doc(value.id).collection('subject').add({
+                      'name': element.name,
+                      'staff': element.staff,
+                    });
+                  });
+                });
+
+                Navigator.pop(context);
+              },
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: ShapeDecoration(
+                  color: Color(0xFF8CC1A9),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(width: 1, color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
-              ),
-              child: Center(
-                child: Text(
-                  'Create course',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
+                child: Center(
+                  child: Text(
+                    'Create course',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
